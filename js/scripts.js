4dc77257b24a9dbef2444fd2ef6817e4374a3166
Stiven Muñoz Pulgarin <1000888016@colcomercio.com.co>
2021-12-17 19:44:04 -0500

    Solucion de errores de diseño y animaciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,194 +1,124 @@
-
-;(function ($) {
-
+(function ($) {
 	'use strict';
-	
- // SCROLL TO TOP
   
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop() > 70) {
-        $('.backtop').addClass('reveal');
-    } else {
-        $('.backtop').removeClass('reveal');
-    }
-});
- 
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-
+	// Sticky Menu
+	$(window).scroll(function () {
+	  if ($('.navigation').offset().top > 100) {
+		$('.navigation').addClass('nav-bg');
+	  } else {
+		$('.navigation').removeClass('nav-bg');
+	  }
 	});
-
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		slidesToShow: 6,
-		slidesToScroll: 6,
-		autoplaySpeed: 6000,
-		responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow:6,
-		        slidesToScroll: 6,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 900,
-		      settings: {
-		        slidesToShow:4,
-		        slidesToScroll: 4
-		      }
-		    },{
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 4,
-		        slidesToScroll: 4
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
-		      }
-		    }
-		  
-  		]
+  
+	// team slider
+	$('.team-slider').slick({
+	  dots: false,
+	  infinite: false,
+	  speed: 1000,
+	  autoplay: true,
+	  autoplaySpeed: 5000,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-arrow-left\'></i></button>',
+	  nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-arrow-right\'></i></button>',
+	  responsive: [{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+	  ]
 	});
-
-	$('.testimonial-wrap').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		dots: true,
-		arrows: false,
-		autoplay: true,
-		vertical:true,
-		verticalSwiping:true,
-		autoplaySpeed: 6000,
-		responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow:1,
-		        slidesToScroll: 1,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 900,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },{
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-		  
-  		]
+  
+	// clients logo slider
+	$('.client-logo-slider').slick({
+	  infinite: true,
+	  slidesToShow: 5,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  dots: false,
+	  arrows: false,
+	  responsive: [{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 400,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+	  ]
 	});
-
-	$('.testimonial-wrap-2').slick({
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		infinite: true,
-		dots: true,
-		arrows:false,
-		autoplay: true,
-		autoplaySpeed: 6000,
-		responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow:2,
-		        slidesToScroll:2,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 900,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },{
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-		  
-  		]
+  
+	// about video popup
+	$(document).ready(function () {
+	  $('.venobox').venobox();
 	});
-
-
-
-	var map;
-
-	function initialize() {
-		var mapOptions = {
-			zoom: 13,
-			center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
-			// styles: style_array_here
-		};
-		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	}
-
-	var google_map_canvas = $('#map-canvas');
-
-	if (google_map_canvas.length) {
-		google.maps.event.addDomListener(window, 'load', initialize);
-	}
-
-	// Counter
-
-	$('.counter-stat span').counterUp({
-	      delay: 10,
-	      time: 1000
-	  });
-
-		
- // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
-
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-})(jQuery);
+  
+	// animation scroll js
+	var html_body = $('html, body');
+	$('.page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+	  if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+		  html_body.animate({
+			scrollTop: target.offset().top - 50
+		  }, 1500, 'easeInOutExpo');
+		  return false;
+		}
+	  }
+	});
+  
+	// easeInOutExpo Declaration
+	jQuery.extend(jQuery.easing, {
+	  easeInOutExpo: function (x, t, b, c, d) {
+		if (t === 0) {
+		  return b;
+		}
+		if (t === d) {
+		  return b + c;
+		}
+		if ((t /= d / 2) < 1) {
+		  return c / 2 * Math.pow(2, 10 * (t - 1));
+		} + b;
+		return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	  }
+	});
+  
+  })(jQuery);
